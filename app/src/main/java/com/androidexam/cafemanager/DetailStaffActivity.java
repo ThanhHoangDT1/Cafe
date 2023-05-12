3665a70442598e43902af2c456effd90dd52537b
nghiahuynhvan <nghiahuynh1102@gmail.com>
2023-05-12 16:31:54 +0700

    quan ly nhan vien

---
+++ app/src/main/java/com/androidexam/cafemanager/DetailStaffActivity.java
@@ -0,0 +1,14 @@
+package com.androidexam.cafemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailStaffActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_staff);
+    }
+}
